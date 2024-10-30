a296f16eabbe7e0e80197c8652bec494611384b8
papinwit-l <pae.papinwit@gmail.com>
2024-10-30 15:15:23 +0700

    add send message state

--- src/controllers/socket-controller.js
+++ src/controllers/socket-controller.js
@@ -1,3 +1,4 @@
+const { PrismaClientExtends } = require("@prisma/client/extension");
 const prisma = require("../config/prisma");
 
 module.exports.disconnect = (socket) => async (data) => {
@@ -41,4 +42,62 @@ module.exports.identify = (socket, io) => async (data) => {
   socket.join(chat.id);
 
   io.to(chat.id).emit("user_joined-" + userId, { message: "User joined" });
+};
+
+module.exports.sendMessage = (socket, io) => async (data) => {
+  const userId = data.userId;
+  const chatId = data.chatId;
+  const message = data.message;
+  if (!userId) {
+    socket.emit("error", { message: "User ID is required" });
+    return;
+  }
+  if (!chatId) {
+    socket.emit("error", { message: "Chat ID is required" });
+    return;
+  }
+  if (!message) {
+    socket.emit("error", { message: "Message is required" });
+    return;
+  }
+  const user = await prisma.user.findUnique({
+    where: {
+      id: +userId,
+    },
+  });
+  if (!user) {
+    socket.emit("error", { message: "User not found" });
+    return;
+  }
+  const chat = await prisma.chat.findUnique({
+    where: {
+      id: +chatId,
+    },
+  });
+  if (!chat) {
+    socket.emit("error", { message: "Chat not found" });
+    return;
+  }
+  const chatMember = await prisma.chatMember.findFirst({
+    where: {
+      userId: user.id,
+      chatId: chat.id,
+    },
+  });
+  if (!chatMember) {
+    socket.emit("error", { message: "User not found" });
+    return;
+  }
+  const newMessage = await prisma.message.create({
+    data: {
+      userId: user,
+      chatId: chat.id,
+      message: message,
+    },
+  });
+  io.to(chat.id).emit("new_message", {
+    message: newMessage,
+    user: user,
+    chatId: chat.id,
+  });
 };
