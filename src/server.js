3ac84e7ecb571c2169b244820e1f1e0840eb706f
chonlathanme <chlmethanan@gmail.com>
2024-10-30 13:35:21 +0700

    Feature booking first commit

--- src/server.js
+++ src/server.js
@@ -2,12 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const { readdirSync } = require("fs");
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
+
+readdirSync("./src/routes").map((path) => app.use("/", require(`./routes/${path}`)));
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
