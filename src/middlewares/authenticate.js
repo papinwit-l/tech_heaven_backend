67bfc841621fd00ae1b3e90a6cebb4f725a6ec01
papinwit-l <pae.papinwit@gmail.com>
2024-10-31 15:23:41 +0700

    fix authen middleware createError

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,36 +1,24 @@
-const jwt = require("jsonwebtoken")
-const createError = require('../utils/createError');
-const prisma = require('../config/prisma');
+const jwt = require("jsonwebtoken");
+const createError = require("../utils/createError");
+const prisma = require("../config/prisma");
 
-
-module.exports.auth = (async(req,res,next)=>{
-        const authorization = req.headers.authorization;
-        if (!authorization || !authorization.startsWith('Bearer ')) {
-          createError({
-            message: 'unauthenticated',
-            statusCode: 401
-          });
-        }
-        const accessToken = authorization.split(' ')[1];
-        if(accessToken === null){
-            createError({
-                message: 'unauthenticated',
-                statusCode: 401
-              });
-              return
-        }
-        // console.log("object",accessToken)
-        const payload = jwt.verify(accessToken, process.env.JWT_SECRET);
-        // console.log(payload,'111111111111111111111111111111')
-        const user = await prisma.user.findUnique({ where: { id: payload.user.id } });
-        if (!user) {
-          createError({
-            message: 'user was not found',
-            statusCode: 400
-          });
-        }
-        delete user.password;
-        req.user = user;
-        next();
-   
-})
+module.exports.auth = async (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return createError(401, "unauthenticated");
+  }
+  const accessToken = authorization.split(" ")[1];
+  if (accessToken === null) {
+    return createError(401, "unauthenticated");
+  }
+  // console.log("object",accessToken)
+  const payload = jwt.verify(accessToken, process.env.JWT_SECRET);
+  // console.log(payload,'111111111111111111111111111111')
+  const user = await prisma.user.findUnique({ where: { id: payload.user.id } });
+  if (!user) {
+    createError(401, "unauthenticated user not found");
+  }
+  delete user.password;
+  req.user = user;
+  next();
+};
