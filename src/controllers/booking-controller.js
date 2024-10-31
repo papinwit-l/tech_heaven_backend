4529ac2525104635ff4ec7326a976a753f8efb26
chonlathanme <chlmethanan@gmail.com>
2024-10-31 15:29:55 +0700

    feature booking done

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -2,25 +2,33 @@ const prisma = require("../models/prisma");
 const trycatch = require("../utils/try-catch");
 
 exports.createBooking = trycatch(async (req, res) => {
-    console.log(req.body)
-    const { bookingDate, type, notes, status, userId } = req.body
-    const booking = await prisma.booking.create({
-        data: {
-            userId: {
-                connect: {
-                    id: Number(id)
-                }
+    console.log(req.body);
+    const { bookingDate, type, notes, status } = req.body;
+    if (!req.user || !req.user.id) {
+        return res.status(401).send("User not authenticated");
+    }
+    try {
+        const booking = await prisma.booking.create({
+            data: {
+                user: {
+                    connect: {
+                        id: +req.user.id,
+                    },
+                },
+                bookingDate,
+                status,
+                type,
+                notes,
             },
-            bookingDate,
-            status,
-            type,
-            notes,
-        }
-    })
+        });
 
-    res.send("Booking created")
-    console.log(booking)
-})
+        res.status(201).send("Booking created");
+        console.log(booking);
+    } catch (error) {
+        console.error("Error creating booking:", error);
+        res.status(500).send("Error creating booking");
+    }
+});
 
 exports.getAllBookings = trycatch(async (req, res) => {
     const { count } = req.params
--- src/routes/booking-route.js
+++ src/routes/booking-route.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const bookRouter = express.Router();
 const bookController = require("../controllers/booking-controller");
+const authenticate = require("../middlewares/authenticate");
 
-bookRouter.post("/create-booking", bookController.createBooking)
+bookRouter.post("/create-booking", authenticate.auth, bookController.createBooking)
 bookRouter.get("/get-all-bookings/:count", bookController.getAllBookings)
 bookRouter.get("/get-booking-by-id/:bookingId", bookController.getBookingById)
 bookRouter.patch("/update-booking/:bookingId", bookController.updateBooking)
