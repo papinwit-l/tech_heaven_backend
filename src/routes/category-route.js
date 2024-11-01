629638fea2e639c400b186c519ea4ede9cb19049
Samach-C <samach.oodz@gmail.com>
2024-11-01 17:38:01 +0700

    categorycomplete

--- src/routes/category-route.js
+++ src/routes/category-route.js
@@ -2,10 +2,11 @@ const express = require('express')
 const router = express.Router()
 const Authenticate = require("../middlewares/authenticate")
 // Controllers
-const { createCategory, listCategory, removeCategory } = require('../controllers/category-controller')
+const { createCategory, listCategory, removeCategory, updateCategory } = require('../controllers/category-controller')
 
 router.post("/category",Authenticate.auth, createCategory)
 router.get("/category", listCategory)
+router.put("/category/:id", Authenticate.auth, updateCategory)
 router.delete("/category/:id",Authenticate.auth, removeCategory)
 
 module.exports = router
