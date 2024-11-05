2685a4faa6ef587b286f534c4bb5e581fa95dead
papinwit-l <pae.papinwit@gmail.com>
2024-11-05 17:05:02 +0700

    chat notify

--- src/controllers/chat-controller.js
+++ src/controllers/chat-controller.js
@@ -125,3 +125,88 @@ module.exports.getChatById = async (req, res, next) => {
     next(error);
   }
 };
+
+module.exports.getNotification = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const chatMember = await prisma.chatMember.findFirst({
+      where: {
+        userId: +userId,
+      },
+    });
+    const chatNotify = await prisma.chatNotification.findMany({
+      where: {
+        chatId: +chatMember.chatId,
+        isRead: false,
+      },
+      include: {
+        message: {
+          select: {
+            id: true,
+            createdAt: true,
+            updatedAt: true,
+            chatId: true,
+            userId: true,
+            user: {
+              select: {
+                id: true,
+                firstName: true,
+                lastName: true,
+                email: true,
+                profileImage: true,
+                role: true,
+              },
+            },
+          },
+        },
+      },
+    });
+    res.json(chatNotify);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+module.exports.adminGetNotification = async (req, res, next) => {
+  try {
+    if (req.user.role !== "ADMIN") {
+      return createError(401, "User not admin");
+    }
+    const chatNotify = await prisma.chatNotification.findMany({
+      where: {
+        isAdminRead: false,
+        message: {
+          user: {
+            role: "USER",
+          },
+        },
+      },
+      include: {
+        message: {
+          select: {
+            id: true,
+            createdAt: true,
+            updatedAt: true,
+            chatId: true,
+            userId: true,
+            user: {
+              select: {
+                id: true,
+                firstName: true,
+                lastName: true,
+                email: true,
+                profileImage: true,
+                role: true,
+              },
+            },
+          },
+        },
+      },
+    });
+    res.json(chatNotify);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
--- src/controllers/socket-controller.js
+++ src/controllers/socket-controller.js
@@ -108,70 +108,6 @@ module.exports.joinChat = (socket, io) => async (data) => {
   console.log(`user ${userId} join chat: ${chatId}`);
 };
 
-//==== SEND MESSAGE ====//
-module.exports.sendMessage = (socket, io) => async (data) => {
-  const userId = data.userId;
-  const chatId = data.chatId;
-  const message = data.message;
-  if (!userId) {
-    socket.emit("error", { message: "User ID is required" });
-    return;
-  }
-  if (!chatId) {
-    socket.emit("error", { message: "Chat ID is required" });
-    return;
-  }
-  if (!message) {
-    socket.emit("error", { message: "Message is required" });
-    return;
-  }
-  const user = await prisma.user.findUnique({
-    where: {
-      id: +userId,
-    },
-  });
-  if (!user) {
-    socket.emit("error", { message: "User not found" });
-    return;
-  }
-  const chat = await prisma.chat.findUnique({
-    where: {
-      id: +chatId,
-    },
-  });
-  if (!chat) {
-    socket.emit("error", { message: "Chat not found" });
-    return;
-  }
-  const chatMember = await prisma.chatMember.findFirst({
-    where: {
-      userId: user.id,
-      chatId: chat.id,
-    },
-  });
-  if (!chatMember) {
-    socket.emit("error", { message: "User not found" });
-    return;
-  }
-  const newMessage = await prisma.message.create({
-    data: {
-      userId: user,
-      chatId: chat.id,
-      message: message,
-    },
-  });
-  io.to(chat.id).emit("new_message", {
-    message: newMessage,
-    user: user,
-    chatId: chat.id,
-  });
-  socket.emit("notify-" + chatId, {
-    message: newMessage,
-    chatId: chat.id,
-    user: user,
-  });
-};
-
 //==== USER SEND MESSAGE ====//
 module.exports.userSendMessage = (socket, io) => async (data) => {
   const userId = data.userId;
@@ -244,6 +180,7 @@ module.exports.userSendMessage = (socket, io) => async (data) => {
     message: newMessage,
     chatId: chat.id,
   });
+  chatNotify(io, chat.id, newMessage, user.role);
 };
 
 //==== ADMIN SEND MESSAGE ====//
@@ -312,4 +249,147 @@ module.exports.adminSendMessage = (socket, io) => async (data) => {
     message: newMessage,
     chatId: chat.id,
   });
+  //notify user
+  chatNotify(io, chat.id, newMessage, user.role);
+};
+
+//==== Chat Notify ====//
+const chatNotify = async (io, chatId, message, senderRole) => {
+  try {
+    console.log("call ChatNotify");
+    //check notify exist
+    const notify = await prisma.chatNotification.findFirst({
+      where: {
+        chatId: chatId,
+      },
+    });
+    // ==== if not exist create new notify ====
+    if (!notify) {
+      //if sender role is admin isAdminRead is true but if sender role is user isRead is true
+      const newNotify = await prisma.chatNotification.create({
+        data: {
+          chatId: chatId,
+          isRead: senderRole === "ADMIN" ? false : true,
+          isAdminRead: senderRole === "ADMIN" ? true : false,
+          messageId: message.id,
+        },
+        include: {
+          message: {
+            select: {
+              id: true,
+              message: true,
+              createdAt: true,
+              updatedAt: true,
+              user: {
+                select: {
+                  id: true,
+                  firstName: true,
+                  lastName: true,
+                  profileImage: true,
+                  email: true,
+                  role: true,
+                },
+              },
+            },
+          },
+        },
+      });
+      io.to(chatId).emit("chatNotify", {
+        notify: newNotify,
+        chatId: chatId,
+      });
+      return;
+    }
+    // ==== if exist update notify ====
+    //if sender role is admin isAdminRead is true but if sender role is user isRead is true
+    const updateNotify = await prisma.chatNotification.update({
+      where: {
+        id: notify.id,
+      },
+      data: {
+        isRead: senderRole === "ADMIN" ? false : true,
+        isAdminRead: senderRole === "ADMIN" ? true : false,
+        messageId: message.id,
+      },
+      include: {
+        message: {
+          select: {
+            id: true,
+            message: true,
+            createdAt: true,
+            updatedAt: true,
+            user: {
+              select: {
+                id: true,
+                firstName: true,
+                lastName: true,
+                profileImage: true,
+                email: true,
+                role: true,
+              },
+            },
+          },
+        },
+      },
+    });
+    io.to(chatId).emit("chatNotify", {
+      notify: updateNotify,
+      chatId: chatId,
+    });
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+};
+
+module.exports.updateChatNotify = (socket, io) => async (data) => {
+  console.log("call updateChatNotify");
+  const userId = data.userId;
+  const chatId = data.chatId;
+  if (!userId) {
+    socket.emit("error", { message: "User ID is required" });
+    return;
+  }
+  if (!chatId) {
+    socket.emit("error", { message: "Chat ID is required" });
+    return;
+  }
+  const user = await prisma.user.findUnique({
+    where: {
+      id: +userId,
+    },
+  });
+  if (!user) {
+    socket.emit("error", { message: "User not found" });
+    return;
+  }
+  const chatNotify = await prisma.chatNotification.findFirst({
+    where: {
+      chatId: chatId,
+    },
+  });
+  if (user.role === "ADMIN") {
+    const updateNotify = await prisma.chatNotification.update({
+      where: {
+        id: chatNotify.id,
+      },
+      data: {
+        isAdminRead: true,
+      },
+    });
+    //notify other admin
+    io.to(chatId).emit("updateAdminChatNotify", {
+      notify: updateNotify,
+      chatId: chatId,
+    });
+  } else {
+    const updateNotify = await prisma.chatNotification.update({
+      where: {
+        id: chatNotify.id,
+      },
+      data: {
+        isRead: true,
+      },
+    });
+  }
 };
